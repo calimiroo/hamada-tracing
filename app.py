97363b6f34e03359c11efcc8690d91ed83b5830a
calimiroo <salam_al7abayeb@yahoo.com>
2026-01-16 15:55:28 +0400

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,8 @@ def get_driver():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')  # إضافة للبيئات السحابية
+    options.add_argument('--remote-debugging-port=9222')  # لتجنب تعارض المنافذ
     return uc.Chrome(options=options, headless=True, use_subprocess=False)
 def color_status(val):
     color = '#90EE90' if val == 'Found' else '#FFCCCB'
